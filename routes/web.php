d8a7aa71e496d8be88bb06949d1dd84c23d9fec0
koizumi taichi <taichi-koizumi@gmo.jp>
2017-04-12 20:19:21 +0900

    Init

---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Task;
+use Illuminate\Http\Request;
+
+/*
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
+
+/**
+ * Show Task Dashboard
+ */
+Route::get('/', function () {
+    
+    $task = Task::orderBy('created_at', 'asc')->get();
+    return view('tasks', [
+        'tasks' => $task
+    ]);
+});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+/**
+ * Add New Task
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+                'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+                        ->withInput()
+                        ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * Delete Task
+ */
+Route::delete('/task/{id}', function ($id) {
+    Task::findOrFail($id)->delete();
+
+    return redirect('/');
+});
